5698d03c4534b5fb8859fc06e815b955c5ae9523
emreprlkk <eememreprlk@gmail.com>
2024-08-16 18:00:29 +0300

    #

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import { Grid, Card, Typography } from '@mui/material';
+import { Grid, Card, Typography, Button } from '@mui/material';
 import { styled } from '@mui/system';
+import './flipcard.css'
+
+
+const colorsFront = ['#7fff00', '#6495ed', '#dc143c', 'violet ', '#FF8C33', 'lightpink ']; // Belirli renkler
+const colorsBack = ['#fff8dc']; // Belirli renkler
 
 const CardContainer = styled(Card)(({ theme }) => ({
-  height: 300, // Kartın yüksekliğini artırdık
+  width: 300,
+  height: 200,
   perspective: 1000,
+  margin: '1px',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  transition: 'all 0.3s ease-in-out',
+
 }));
 
 const FlipCardInner = styled('div')(({ isFlipped }) => ({
@@ -17,49 +29,55 @@ const FlipCardInner = styled('div')(({ isFlipped }) => ({
   transform: isFlipped ? 'rotateY(180deg)' : 'rotateY(0deg)',
 }));
 
-const FlipCardFront = styled('div')({
+const FlipCardFront = styled('div')(({ color }) => ({
   position: 'absolute',
   width: '100%',
   height: '100%',
   backfaceVisibility: 'hidden',
-  backgroundColor: '#1a237e',
-  color: 'white',
+  backgroundColor: color, // Ön yüz rengi dinamik olarak belirlenir
+  color: 'black',
   display: 'flex',
   justifyContent: 'center',
   alignItems: 'center',
-  borderRadius: '8px',
-  textAlign:'center'
+  textAlign:'center',
+  borderRadius: '10px',
+  boxSizing: 'border-box'
+}));
 
-});
-
-const FlipCardBack = styled('div')({
+const FlipCardBack = styled('div')(({ color }) => ({
   position: 'absolute',
   width: '100%',
   height: '100%',
   backfaceVisibility: 'hidden',
-  backgroundColor: '#fffaf0',
+  backgroundColor: color, // Arka yüz rengi dinamik olarak belirlenir
   color: 'black',
   transform: 'rotateY(180deg)',
   display: 'flex',
+  fontSize:'12px',
   justifyContent: 'center',
   alignItems: 'center',
   textAlign:'center',
   borderRadius: '8px',
   padding: '10px',
   boxSizing: 'border-box',
+}));
 
-});
 
 const TextContainer = styled('div')({
   maxHeight: '100%', // Kartın yüksekliğine göre sınırlandır
   overflowY: 'auto', // Uzun metinler için kaydırma işlevi
   width: '100%', // Metnin taşmaması için genişliği sınırlıyoruz
-  paddingRight: '10px', // Kaydırma çubuğu ile metin arasında boşluk bırakıyoruz
+  paddingRight: '20px', // Kaydırma çubuğu ile metin arasında boşluk bırakıyoruz
 });
 
 const App = () => {
   const [items, setItems] = useState([]);
   const [flippedIndex, setFlippedIndex] = useState(null);
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 18;
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const currentItems = items.slice(indexOfFirstItem, indexOfLastItem);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -73,32 +91,69 @@ const App = () => {
 
     fetchData();
   }, []);
-
+  const pageNumbers = [];
+  for (let i = 1; i <= Math.ceil(items.length / itemsPerPage); i++) {
+    pageNumbers.push(i);
+  }
   const handleFlip = (index) => {
     setFlippedIndex(flippedIndex === index ? null : index);
   };
 
   return (
-    <Grid container spacing={2} padding={2}>
-      {items.map((item, index) => (
-        <Grid item xs={12} sm={6} md={4} lg={3} xl={2} key={index}>
-          <CardContainer onClick={() => handleFlip(index)}>
-            <FlipCardInner isFlipped={flippedIndex === index}>
-              <FlipCardFront>
-                <Typography variant="h5">{item.Kelime}</Typography>
-              </FlipCardFront>
-              <FlipCardBack>
+    <div >
+      <div className='flipcard'>  
+      
+       <Grid   container spacing={{ xs: 1, md: 1,l:1,xl:1}} columns={{ xs: 1, sm: 1, md: 1 ,l:1,xl:1}}>
+        {currentItems.map((item, index) => (
+           <Grid item  key={index}>
+            <CardContainer onClick={() => handleFlip(index)}>
+              <FlipCardInner isFlipped={flippedIndex === index}>
+                <FlipCardFront color={colorsFront[index % colorsFront.length]}>
+                  <Typography variant="h4">{item.Kelime}</Typography>
+                </FlipCardFront>
+                <FlipCardBack color={colorsBack[(index + 1) % colorsBack.length]}>
                 <TextContainer>
                   <Typography variant="body1">
                     {item.Anlamı}
                   </Typography>
-                </TextContainer>
-              </FlipCardBack>
-            </FlipCardInner>
-          </CardContainer>
-        </Grid>
-      ))}
-    </Grid>
+                  </TextContainer>
+                </FlipCardBack>
+              </FlipCardInner>
+            </CardContainer>
+          </Grid>
+        ))}
+      </Grid>
+      <div className='pagination'> 
+         {pageNumbers.map(number => (
+          <Button
+            key={number}
+            variant={number === currentPage ? 'contained' : 'outlined'}
+            onClick={() => setCurrentPage(number)} >
+            {number}
+          </Button>
+        ))}
+      
+    </div>
+      
+      
+      </div>
+      {/*    <div className='pagination'> 
+         {pageNumbers.map(number => (
+          <Button
+            key={number}
+            variant={number === currentPage ? 'contained' : 'outlined'}
+            onClick={() => setCurrentPage(number)} >
+            {number}
+          </Button>
+        ))}
+      
+    </div>*/}
+   
+
+
+      
+     
+    </div>
   );
 };
 
